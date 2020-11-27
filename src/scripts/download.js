9f60220d8c5d156d570a08920a2922224b6088a5
hackerhgl <hackerhgl@gmail.com>
2020-11-27 20:58:00 +0500

    Improved toggle and PDF download option

--- src/scripts/download.js
+++ src/scripts/download.js
@@ -3,13 +3,28 @@ window.isToggled = false;
 window.toggle = function() {
   const download = document.getElementById("download-card");
   const footer = document.getElementById("footer");
+  const header = document.getElementById("header");
+  const contact = document.getElementById("contact");
+  const skills = document.getElementById("skills");
+
+  const elementsToHide = [download, footer, header];
+  const elementsClass = [contact, skills];
+
   if (!window.isToggled) {
     window.isToggled = true;
-    download.style.display = "none";
-    footer.style.display = "none";
+    elementsToHide.forEach((element) => {
+      element.style.display = "none";
+    });
+    elementsClass.forEach((element) => {
+      element.classList.add('toggled');
+    })
   } else {
     window.isToggled = false;
-    download.style.display = "block";
-    footer.style.display = "block";
+    elementsToHide.forEach((element) => {
+      element.style.display = "block";
+    });
+    elementsClass.forEach((element) => {
+      element.classList.remove('toggled');
+    })
   }
 }
