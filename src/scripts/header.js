478d297a9f47a5cb91c2d7277dc936eea8ea22bb
hackerhgl <hackerhgl@gmail.com>
2021-06-05 20:28:13 +0500

    Initial dimensoins

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,8 +1,70 @@
 const header = document.getElementById("header");
 
-window.addEventListener('scroll', () => {
-  const offset = document.body.scrollTop || document.documentElement.scrollTop;
 
-  header.style.backgroundPositionY = `${offset * -0.9}px`;
 
-});
+(function() {
+    var c = document.getElementById('canvas'),
+            ctx = canvas.getContext('2d');
+
+    // resize the canvas to fill browser window dynamically
+    window.addEventListener('resize', resizeCanvas, false);
+
+    function resizeCanvas() {
+            c.width = window.innerWidth - getScrollbarWidth();
+            c.height = 450;
+            drawStuff();
+    }
+    resizeCanvas();
+
+    function drawStuff() {
+      const center = {x: c.width/2, y: c.height/2};
+      const initialFrame = {
+        start: {
+          xs: center.x - (c.width * 0.14),
+          ys: center.y - (c.height * 0.14),
+          xe: center.x + (c.width * 0.14),
+          ye: center.y + (c.height * 0.14),
+        },
+        end: {
+          xs: center.x - (c.width * 0.05),
+          ys: center.y - (c.height * 0.05),
+          xe: center.x + (c.width * 0.05),
+          ye: center.y + (c.height * 0.05),
+        },
+      }
+      console.log(initialFrame);
+
+    }
+})();
+
+// window.addEventListener('scroll', () => {
+//   console.log(canvas.clientWidth, canvas.clientHeight);
+//   const offset = document.body.scrollTop || document.documentElement.scrollTop;
+
+
+// });
+
+
+
+function getScrollbarWidth() {
+
+  // Creating invisible container
+  const outer = document.createElement('div');
+  outer.style.visibility = 'hidden';
+  outer.style.overflow = 'scroll'; // forcing scrollbar to appear
+  outer.style.msOverflowStyle = 'scrollbar'; // needed for WinJS apps
+  document.body.appendChild(outer);
+
+  // Creating inner element and placing it in the container
+  const inner = document.createElement('div');
+  outer.appendChild(inner);
+
+  // Calculating difference between container's full width and the child width
+  const scrollbarWidth = (outer.offsetWidth - inner.offsetWidth);
+
+  // Removing temporary elements from the DOM
+  outer.parentNode.removeChild(outer);
+
+  return scrollbarWidth;
+
+}
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,5 +3,9 @@ import './header.js';
 import './download.js';
 
 window.onload = function () {
+  // const c = document.getElementById("canvas");
+  // c.width = window.innerWidth;
+  // c.height = 450;
+
   SmoothScroll(document,120,12);
 }
