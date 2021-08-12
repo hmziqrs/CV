e07f92e0249feb9f48e0f78868003b96f5e916cb
hackerhgl <hackerhgl@gmail.com>
2021-08-13 02:04:19 +0500

    Skill icons animation working

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,14 +1,21 @@
 const ITEM_CLASS = "skill-anim-base";
 const ACTIVE = "active";
+const REMOVE = "remove";
 let active = 0;
+let timeout;
 
-
-function init(params) {
+function init() {
   const elements = document.getElementsByClassName(ITEM_CLASS);
   elements[0].classList.add(ACTIVE);
 
   setInterval(() => {
+    clearTimeout(timeout);
     elements[active].classList.remove(ACTIVE);
+    elements[active].classList.add(REMOVE);
+    timeout = setTimeout(() => {
+      elements[active].classList.remove(REMOVE);
+      clearTimeout(timeout);
+    }, 2000);
     active++;
     elements[active].classList.add(ACTIVE);
   }, 4000);
