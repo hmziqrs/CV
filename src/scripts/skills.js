c39d1f63f350001b14c51cdcd0e4272e94a60103
hackerhgl <hackerhgl@gmail.com>
2021-08-13 02:31:52 +0500

    Fixed animation logic

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -2,26 +2,38 @@ const ITEM_CLASS = "skill-anim-base";
 const ACTIVE = "active";
 const REMOVE = "remove";
 let active = 0;
-let timeout;
+let length = 0;
 
 function init() {
   const elements = document.getElementsByClassName(ITEM_CLASS);
+  length = elements.length;
   elements[0].classList.add(ACTIVE);
 
   setInterval(() => {
-    clearTimeout(timeout);
     elements[active].classList.remove(ACTIVE);
     elements[active].classList.add(REMOVE);
-    timeout = setTimeout(() => {
-      elements[active].classList.remove(REMOVE);
-      clearTimeout(timeout);
-    }, 2000);
-    active++;
-    if (active >= elements.length) {
-      active = 0;
-    }
+    setTimeout(() => {
+      elements[prev()].classList.remove(REMOVE);
+    }, 1200);
+      active = next();
     elements[active].classList.add(ACTIVE);
   }, 4000);
+}
+
+function prev() {
+  const no = active-1;
+  if (active <= 0) {
+    return length -1;
+  }
+  return no;
+}
+
+function next() {
+  const no = active +1;
+  if (no >= length) {
+    return 0;
+  }
+  return no;
 }
 
 
