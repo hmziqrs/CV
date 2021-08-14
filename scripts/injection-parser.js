40e84ee2cd1437de395d98d07c1afa1e7ad3f471
hackerhgl <hackerhgl@gmail.com>
2021-08-15 01:42:14 +0500

    Added skills w/icons data in experience

--- scripts/injection-parser.js
+++ scripts/injection-parser.js
@@ -1,9 +1,9 @@
-
+const isString = require('lodash/isString');
+const skillsData = require(`./data/skills.json`);
 
 function skills(data) {
-  const json = require(`./data/skills.json`);
   const array = [];
-  for (value of json) {
+  for (value of skillsData) {
     for (element of value.skills) {
       array.push(element);
     }
@@ -11,4 +11,24 @@ function skills(data) {
   return `${data} var skills = ${JSON.stringify(array)};`;
 }
 
-module.exports = {skills}
+function strToSkill(str) {
+  for (section of skillsData) {
+    for (skill of section.skills) {
+      if (skill.label.toLowerCase() == str.toLowerCase()) {
+        return skill;
+      }
+    }
+  }
+}
+
+function experience(_) {
+  const data = require(`./data/experience.json`).map((obj) => ({
+    ...obj,
+    tech:  obj.tech ? obj.tech.map((t) => isString(t) ? strToSkill(t): t): [],
+  }));
+
+  return `- var data = ${JSON.stringify(data)};`
+
+}
+
+module.exports = {skills, experience}
