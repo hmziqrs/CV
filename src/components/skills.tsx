4a4f26d9905eb7b76c5c928cc7b9018e5dc3ad8c
hmziqrs <hmziqrs@gmail.com>
2024-11-07 02:03:07 +0500

    improved UI

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -4,7 +4,7 @@ export function Skills() {
   return (
     <div className="flex bg-zinc-100 dark:bg-zinc-900">
       <div className="container max-w-6xl mx-auto px-6 py-10">
-        <h2 className="text-2xl font-bold text-zinc-800 dark:text-zinc-100 mb-8">
+        <h2 className="text-2xl font-semibold text-zinc-800 dark:text-zinc-100 mb-8">
           Skills
         </h2>
 
@@ -17,13 +17,14 @@ export function Skills() {
                 {section.skills.map((skill) => (
                   <div
                     key={skill.label}
+                    // sm:px-4 px-4 py-3
                     className="flex items-center sm:gap-3 gap-2 cursor-pointer
-                    sm:px-4 px-4 py-3
+                    py-2 px-4
                     bg-zinc-200 dark:bg-zinc-800
                     hover:bg-zinc-300 dark:hover:bg-zinc-700
                     rounded-full duration-300"
                   >
-                    <skill.icon className="text-zinc-700 dark:text-zinc-300 sm:h-5 h-4" />
+                    <skill.icon className="text-zinc-700 dark:text-zinc-300 sm:w-4 w-4" />
                     <p className="text-xs sm:text-sm font-medium font-mono text-zinc-800 dark:text-zinc-200">
                       {skill.label}
                     </p>
