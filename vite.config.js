64f7bcd9463238cba9f59b30ddbf1ed8b8d3f244
hmziqrs <hmziqrs@gmail.com>
2024-11-06 07:51:37 +0500

    park

--- vite.config.js
+++ vite.config.js
@@ -1,55 +1,6 @@
-import { defineConfig } from "vite";
-import { resolve } from "path";
-import pugPlugin from "vite-plugin-pug";
-import { createRequire } from "module";
+import vituum from "vituum";
+import pug from "@vituum/vite-plugin-pug";
 
-const require = createRequire(import.meta.url);
-
-// Load data for pug templates
-const loadData = () => {
-  const data = {};
-  const components = [
-    "community",
-    "contact",
-    "development",
-    "download",
-    "experience",
-    "projects",
-    "skills",
-    "story",
-    "summary",
-  ];
-
-  for (const component of components) {
-    data[component] = require(`./scripts/data/${component}.json`);
-  }
-
-  return data;
+export default {
+  plugins: [vituum(), pug({})],
 };
-
-export default defineConfig({
-  root: "src",
-  build: {
-    outDir: "../build",
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "src/index.pug"),
-      },
-    },
-  },
-  plugins: [
-    pugPlugin({
-      localImports: true,
-      pretty: true,
-      data: loadData(),
-    }),
-  ],
-  css: {
-    preprocessorOptions: {
-      sass: {
-        quietDeps: true,
-      },
-    },
-  },
-});
