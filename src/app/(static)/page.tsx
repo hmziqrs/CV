af845e4510b7e2e5d181a784e5c47bb5bf9dfd86
hmziqrs <hmziqrs@gmail.com>
2024-11-07 13:28:31 +0500

    lint fix

--- src/app/(static)/page.tsx
+++ src/app/(static)/page.tsx
@@ -3,7 +3,6 @@ import { Experience } from "@/components/experience";
 import { Footer } from "@/components/footer";
 import { Header } from "@/components/header";
 import { Projects } from "@/components/projects";
-import { PuppeteerHelper } from "@/components/puppeteer-helper";
 import { Skills } from "@/components/skills";
 export default function Home() {
   return (
